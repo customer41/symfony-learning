a70fba362853321050ec0bf3133122ab0b03bc66
Alexander Popov <alien1986cs@gmail.com>
2025-03-14 12:10:03 +0300

    fix exception listener

--- src/Application/EventListener/KernelExceptionEventListener.php
+++ src/Application/EventListener/KernelExceptionEventListener.php
@@ -2,48 +2,57 @@
 
 namespace App\Application\EventListener;
 
+use App\Controller\DTO\ErrorResponse;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Exception\ValidationFailedException;
 
 #[AsEventListener(event: ExceptionEvent::class)]
 class KernelExceptionEventListener
 {
+    public function __construct(private readonly SerializerInterface $serializer) {}
+
     public function onKernelException(ExceptionEvent $event): void
     {
         $exception = $event->getThrowable();
 
         if ($exception instanceof HttpExceptionInterface) {
-            if ($exception->getPrevious() !== null) {
-                $exception = $exception->getPrevious();
+            if ($exception->getPrevious() instanceof ValidationFailedException) {
+                $validationFailedMessage = $this->getValidationFailedMessage();
+                $validationErrors = $this->getValidationErrors($exception->getPrevious());
+                $response = $this->getHttpResponse($validationFailedMessage, Response::HTTP_BAD_REQUEST, $validationErrors);
+            } else {
+                $response = $this->getHttpResponse($exception->getMessage(), $exception->getStatusCode());
             }
-        }
 
-        if ($exception instanceof ValidationFailedException) {
-            $event->setResponse($this->getValidationFailedResponse($exception));
-        }
-
-        if ($exception instanceof NotFoundHttpException) {
-            $event->setResponse($this->getDefaultFailedResponse($exception, Response::HTTP_NOT_FOUND));
+            $event->setResponse($response);
         }
     }
 
-    private function getValidationFailedResponse(ValidationFailedException $exception): Response
+    private function getHttpResponse(string $message, int $code, array $details = []): Response
     {
-        $response = [];
+        $errorResponse = new ErrorResponse($message, $details);
+        $responseData = $this->serializer->serialize($errorResponse, JsonEncoder::FORMAT);
+
+        return new Response($responseData, $code, ['Content-Type' => 'application/json']);
+    }
+
+    private function getValidationErrors(ValidationFailedException $exception): array
+    {
+        $errors = [];
         foreach ($exception->getViolations() as $violation) {
-            $response[$violation->getPropertyPath()] = $violation->getMessage();
+            $errors[$violation->getPropertyPath()][] = $violation->getMessage();
         }
 
-        return new JsonResponse($response, Response::HTTP_BAD_REQUEST);
+        return $errors;
     }
 
-    private function getDefaultFailedResponse(HttpExceptionInterface $exception, int $httpCode): Response
+    private function getValidationFailedMessage(): string
     {
-        return new JsonResponse(['success' => false, 'error' => $exception->getMessage()], $httpCode);
+        return 'Validation failed';
     }
 }
