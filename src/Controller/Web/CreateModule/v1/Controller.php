4ec55cf3f866ab26ed5a7a3c184299539aac4752
Alexander Popov <alien1986cs@gmail.com>
2025-03-24 16:07:24 +0300

    RBAC model

--- src/Controller/Web/CreateModule/v1/Controller.php
+++ src/Controller/Web/CreateModule/v1/Controller.php
@@ -7,11 +7,13 @@ use App\Controller\Web\CreateModule\v1\Output\CreatedModuleDTO;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpKernel\Attribute\MapRequestPayload;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class Controller extends AbstractController
 {
     public function __construct(private readonly Manager $manager) {}
 
+    #[IsGranted('ROLE_MANAGER_CRUD_EDU_ENTITY')]
     #[Route(path: '/api/v1/module', name: 'api_v1_create_module', methods: ['POST'])]
     public function __invoke(#[MapRequestPayload] CreateModuleDTO $createModuleDTO): CreatedModuleDTO
     {
